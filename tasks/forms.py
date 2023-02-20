9fe76e13f9a5b0c6c5e3c57f8bae427133c1e72d
Xopxe23 <georgeduday@gmail.com>
2023-02-20 23:56:57 +0300

    Create Model Task and TaskForm

---
+++ tasks/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+
+class TaskForm(ModelForm):
+
+    class Meta:
+        model = Task
+        fields = '__all__'
